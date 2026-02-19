3530799d503c729251b4a07b4c904c46a98ad6ed
danilkrot315-gif <danilkrot315@gmail.com>
2026-02-19 17:01:29 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './public/index.html'
+      }
+    }
+  },
   server: {
     port: 3000,
     open: true
