dc789f8698b6b3838b0677d8b7bbb27740bc5c7d
danilkrot315-gif <danilkrot315@gmail.com>
2026-02-20 09:05:58 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: './volgograd-online/src/main.jsx'
-      }
-    }
-  },
   server: {
     port: 3000,
     open: true
